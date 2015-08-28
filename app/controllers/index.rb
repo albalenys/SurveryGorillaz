4aacfcc0c3891f3db70472d4f218ee840bfd632a
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-08-28 17:56:08 -0400

    refactor changes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,9 +2,3 @@ get '/' do
   @surveys = Survey.all
   erb :'/index'
 end
-
-# <ol>
-#   <% @surveys.each do |survey| %>
-#     <li><a href="/survery/"><%=survey.title %></a></li>
-#   <% end %>
-# </ol>
