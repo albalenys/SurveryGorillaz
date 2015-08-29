7988e16e670c80ec893994560ae466ab10dfaf6f
Kelly Ripple <email": null>
2015-08-29 16:32:53 -0400

    DRY refactor

--- public/js/application.js
+++ public/js/application.js
@@ -13,17 +13,4 @@ $(".header-link").on('click', function(e) {
   });
 });
 
-$(".login-link").on('click', function(e) {
-  e.preventDefault();
-  var $target = $(this)
-  var url = $target.attr('href')
 
-  $.ajax(url).done(function(data) {
-    $(".content").remove();
-    var content = $(data).wrapAll("<section>");
-    var section = content.parent().addClass("content");
-    $("body").append(section);
-  }).fail(function() {
-    alert("Login failed.");
-  });
-});
