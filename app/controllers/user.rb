f1b36cb2abf9c5f631ed512d09c6964b4213b518
Kelly Ripple <email": null>
2015-08-28 20:35:10 -0400

    fix formatting

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -18,9 +18,14 @@ end
 post '/users/login' do
   user = User.find_by(email: params[:user][:email])
   if user && user.authenticate(params[:user][:password])
-    session[:user] = user
+    session[:user_id] = user
     redirect '/'
   else
     error
   end
+end
+
+delete '/users/:id' do
+  session.clear
+  redirect '/'
 end
