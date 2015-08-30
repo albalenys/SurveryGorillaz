543e95807962df15ff9f0c65510376c325572002
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-08-30 17:46:05 -0400

    WIP

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -7,11 +7,3 @@ get '/choices/new' do
   end
 end
 
-post '/choices' do
-  if request.xhr?
-    puts "value"
-    p params[:data]["0"][:value]
-    puts "question"
-    p params[:question]
-  end
-end
